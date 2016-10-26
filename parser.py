1420c35915d2c76342cb2787ed31c5bbaeb3d85c
Egor <yeplastun@gmail.com>
2016-10-27 00:45:05 +0300

    multiprocessing

--- parser.py
+++ parser.py
@@ -1,28 +1,37 @@
 from bs4 import BeautifulSoup
 from requests import get
 from time import sleep
-from math import ceil
+from multiprocessing import Process
 import re
 import shutil
 import os
 
+def walk_site():
+    fin = open('links_all.txt', 'r')
+    fout = open('pics_url.txt', 'w')
+    image_pattern = re.compile("^.*img\/items\/m.*\.jpg$")
+    prefix = "http://www.onlinetrade.ru"
+    #used_imgs=set()
+    
+    for line in fin:
+        url = line
+        print("looking page %s" % url[:-1])
+        page = get(prefix + url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+    
+        imgs = set([x['src'] for x in soup.findAll(
+                attrs={'src': image_pattern})]) #- used_imgs
+    
+        p = Process(target=load_imgs, args=(imgs,fout))
+        p.start()
 
-fin = open('links_all.txt', 'r')
-fout = open('pics_url.txt', 'w')
-image_pattern = re.compile("^.*img\/items\/m.*\.jpg$")
-prefix = "http://www.onlinetrade.ru"
-used_imgs=set()
-cnt = 1
+        del soup
+        del page
+    
+        sleep(1)
 
-for line in fin:
-    url = line
-    print("looking page %s" % url)
-    page = get(prefix + url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    imgs = set([x['src'] for x in soup.findAll(
-            attrs={'src': image_pattern})]) - used_imgs
-
+def load_imgs(imgs, fout):
+    prefix = "http://www.onlinetrade.ru"
     for img in imgs:
         img_link = prefix + img
         response = get(img_link, stream=True)
@@ -32,12 +41,10 @@ for line in fin:
         with open(img[1:], 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
         del response
-        used_imgs.add(img)
+        #used_imgs.add(img)
         fout.write(img_link + "\n")
-        print(str(cnt) + " Saved image: %s" % img[1:])
-        cnt += 1
+        print("\tSaved image: %s" % img[1:])
 
-    del soup
-    del page
 
-    sleep(1)
+if __name__ == "__main__":
+    walk_site()
