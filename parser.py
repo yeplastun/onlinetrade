8548004ea24eeb13790816dc1390bd01e30112f8
Egor <yeplastun@gmail.com>
2016-10-26 23:25:17 +0300

    Init commit

---
+++ parser.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+from requests import get
+from time import sleep
+from math import ceil
+import re
+import shutil
+import os
+ 
+ 
+fin = open('links_all.txt', 'r')
+fout = open('pics_url.txt', 'w')
+image_pattern = re.compile("^.*img\/items\/m.*\.jpg$")
+prefix = "http://www.onlinetrade.ru"
+used_imgs=set()
+cnt = 1
+ 
+for line in fin:
+    url = line
+    page = get(prefix + url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+ 
+    imgs = set([x['src'] for x in soup.findAll(
+            attrs={'src': image_pattern})]) - used_imgs
+ 
+    for img in imgs:
+        img_link = prefix + img
+        response = get(img_link, stream=True)
+        img_dir = img[1:img.rfind("/")]
+        if not os.path.exists(img_dir):
+            os.makedirs(img_dir)
+        with open(img[1:], 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+        used_imgs.add(img)
+        fout.write(img_link + "\n")
+        print(str(cnt) + " Saved image: %s" % img)
+        cnt += 1
+ 
+    del soup
+    del page
+ 
+    sleep(1)
---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo locale-gen ru_RU.UTF-8
+
+sudo apt install python3-pip
+pip3 install requests bs4
+python3 parser.sh
