960a4ef54ee54c5cb46372ae057afd9d4c314af7
Egor <yeplastun@gmail.com>
2016-10-26 23:45:02 +0300

    lol

--- parser.py
+++ parser.py
@@ -5,23 +5,24 @@ from math import ceil
 import re
 import shutil
 import os
- 
- 
+
+
 fin = open('links_all.txt', 'r')
 fout = open('pics_url.txt', 'w')
 image_pattern = re.compile("^.*img\/items\/m.*\.jpg$")
 prefix = "http://www.onlinetrade.ru"
 used_imgs=set()
 cnt = 1
- 
+
 for line in fin:
     url = line
+    print("looking page %s" % s)
     page = get(prefix + url)
     soup = BeautifulSoup(page.content, 'html.parser')
- 
+
     imgs = set([x['src'] for x in soup.findAll(
             attrs={'src': image_pattern})]) - used_imgs
- 
+
     for img in imgs:
         img_link = prefix + img
         response = get(img_link, stream=True)
@@ -35,8 +36,8 @@ for line in fin:
         fout.write(img_link + "\n")
         print(str(cnt) + " Saved image: %s" % img)
         cnt += 1
- 
+
     del soup
     del page
- 
+
     sleep(1)
